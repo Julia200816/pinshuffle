2ce4f8a8c942069fce25fb365c235cc23c3969ed
Julia200816 <thefangirljuliakiedrowicz@gmail.com>
2025-12-19 20:22:46 +0100

    Update pinterest_client.go

--- pinterest_client.go
+++ pinterest_client.go
@@ -85,9 +85,9 @@ func NewClient() *PinterestClient {
 	return &PinterestClient{
 		AppID:       os.Getenv("1478464"),
 		Secret:      os.Getenv("CLIENT_SECRET"),
-		MainURL:     "https://pinshuffle.fly.dev",
+		MainURL:     "https://julias-pinterest-randomizer.osc-fr1.scalingo.io",
 		BaseURL:     "https://api.pinterest.com/v5",
-		RedirectUri: "https://pinshuffle.fly.dev/redirect/",
+		RedirectUri: "https://julias-pinterest-randomizer.osc-fr1.scalingo.io/redirect/",
 		Scopes:      "user_accounts:read,catalogs:read,boards:read,boards:read_secret,pins:read,pins:read_secret",
 		HttpClient: &http.Client{
 			Timeout: 10 * time.Second,
